a04cae4dc749fe942bed46534493547fbc3b8ad1
Samet Yasin Aktürk <78413314+akturksametyasin@users.noreply.github.com>
2022-10-03 15:56:07 +0300

    interfaceDemo

---
+++ interfaceDemo/src/OutsourceWorker.java
@@ -0,0 +1,6 @@
+public class OutsourceWorker implements IWorkable {      //dış çalışan
+    @Override
+    public void work() {
+
+    }
+}
---
+++ interfaceDemo/src/Worker.java
@@ -0,0 +1,16 @@
+public class Worker implements IWorkable, IEatable, IPayable {      //bizim çalışanımız
+    @Override
+    public void work() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void pay() {
+
+    }
+}
