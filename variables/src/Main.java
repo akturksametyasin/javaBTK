1dc47898b16f4bd667e1b265191cd03bb3451381
Samet Yasin Aktürk <78413314+akturksametyasin@users.noreply.github.com>
2022-10-03 15:50:25 +0300

    variables

---
+++ variables/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main (String[] args){
+        int ogrenciSayisi = 10;
+        String mesaj = "Öğrenci sayısı : ";
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+    }
+}
