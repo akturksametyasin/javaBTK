3a37da8fa34f82f6b52d73f9adbac18d9d30db5c
Samet Yasin Aktürk <78413314+akturksametyasin@users.noreply.github.com>
2022-10-05 12:22:45 +0300

    hashMapDemo

---
+++ hashMapDemo/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String,String> sozluk = new HashMap<String,String>();
+        sozluk.put("book","kitap");
+        sozluk.put("table","masa");
+        sozluk.put("computer","bilgisayar");
+        System.out.println(sozluk.size());
+
+        for (String item : sozluk.keySet()) {
+            System.out.println("Eleman-" + item + " Değer-" + sozluk.get(item));
+        }
+
+        sozluk.remove("table");     //sozluk'ten "table"'ı siler.
+        sozluk.clear();     //sozluk'u tamamen siler.
+        System.out.println(sozluk.get("table"));
+    }
+}
